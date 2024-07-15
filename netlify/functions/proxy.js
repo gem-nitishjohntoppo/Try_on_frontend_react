69fd8b351764167cf73e16208bf7b43cca8ed649
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2024-07-15 12:16:40 +0530

    first commit

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -3,13 +3,27 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 exports.handler = async (event, context) => {
-  const proxy = createProxyMiddleware({
-    target: 'http://13.126.15.191:8005',
-    changeOrigin: true,
-    pathRewrite: { '^/.netlify/functions/proxy': '' }
-  });
+  console.log('Event:', event);
 
   return new Promise((resolve, reject) => {
+    const proxy = createProxyMiddleware({
+      target: 'http://13.126.15.191:8005',
+      changeOrigin: true,
+      pathRewrite: { '^/.netlify/functions/proxy': '' },
+      onError: (err, req, res) => {
+        console.error('Proxy error:', err);
+        res.writeHead(502, { 'Content-Type': 'text/plain' });
+        res.end('Bad Gateway');
+        reject(err);
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log('Proxying request to:', proxyReq.getHeader('host'));
+      },
+      onProxyRes: (proxyRes, req, res) => {
+        console.log('Received response from:', proxyRes.headers);
+      }
+    });
+
     proxy(event, context, (result) => {
       resolve(result);
     });
